73785f946307b11537e368f071103a22dce865b0
vinizzio <vinizzio@hotmail.com>
2026-02-27 23:07:38 -0500

    inicial flutter

---
+++ clase/01_hola_mundo.dart
@@ -0,0 +1,16 @@
+void main() {
+  //comentario simple
+/*
+comentario largo
+deber imprimir en consola
+tengo 40 años
+vivo en latacunga
+los fines de semana practico flutter
+*/
+ print ('hola me llamo Vinicio');
+  print('tengo 40 años');
+  print ('vivo en Latacunga');
+  print ('Los fines de semana practico Flutter y bici');
+ 
+}
+
---
+++ tareas/01_tarea.dart
@@ -0,0 +1,14 @@
+void main() {
+  //comentario simple
+  /*
+  deber imprimir en consola
+  tengo 40 años
+  vivo en latacunga
+  los fines de semana practico flutter
+  */
+ print ('hola me llamo Vinicio');
+  print('tengo 40 años');
+  print ('vivo en Latacunga');
+  print ('Los fines de semana practico Flutter y MTB');
+ 
+}
