526ac5fefee3cc8dcfbe6a7fab540fd658994351
Richar Cangui <richarsantiagocangui96@gmail.com>
2026-03-02 21:14:11 -0500

    Add initial Flutter files and implement basic examples for learning

---
+++ DOCENTE/clase/03_datos_compuestos.dart
@@ -0,0 +1,24 @@
+void main() {
+  // List
+  // List, var, final, const
+  // Una lista es como una cesta, y va a tener cosas internamente
+  // Tipo de dato que va a contener la lista
+  // alt+60 >
+  // alt+62 <
+  List<String> listaDeCompras = ["Pera", "Manzana", "Uva"];
+  print(listaDeCompras);
+
+  // Como se posicionan los elementos en una lista
+  // [elemento1, elemento2, elemento3]
+  //      0           1          2
+  print(listaDeCompras[1]);
+
+  // Operadores 
+  // Longitud de la lista
+  print(listaDeCompras.length);
+  // El primer elemento
+  print(listaDeCompras.first);
+  // El último elemento
+  print(listaDeCompras.last);
+
+}
---
+++ DOCENTE/tareas/01_tarea.dart
@@ -0,0 +1,11 @@
+/*
+Esta es la tarea número 1 en donde practicamos 
+1. Hola mundo
+2. Comentarios
+*/
+void main() {
+  print("Hola Mundo");
+  print("Tengo 29 años");
+  print("Vivo en Latacunga");
+  print("Los fines de semana practico flutter");
+}
