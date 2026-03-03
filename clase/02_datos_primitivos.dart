0d421c26216ef787667262cdff1986b82a842d6c
Vinizzio <vinizzio@hotmail.com>
2026-03-02 21:08:13 -0500

    clase 02032026

---
+++ clase/02_datos_primitivos.dart
@@ -0,0 +1,42 @@
+void main() { 
+  //String se declara con S 
+  String saludo = 'Hola Mundo';
+    print (saludo);
+  
+  // Numeros
+  //Entero - int
+  //decimal - double
+  int edad = 40;
+  double precio =12.153; 
+    print (edad);
+    print (precio);
+  // Booleano - bool
+  bool esVerdad = true;
+    print (esVerdad);
+
+  //hola mi nombre es Vinicio y tengo 40 años
+  String nombre ="Vinicio";
+  print ("Hola mi nombre es $nombre y mi edad es $edad años ");
+
+  //Variables, finales, constantes
+  // tipo de dato
+  String apellido ="Cueva";
+  print (apellido);
+
+  //var - es una variable con inferecia de tipos de datos
+  var fruta = "manzanas";
+  print (fruta);
+  fruta = "nueva manzana";
+  print (fruta);
+ // final
+ final precioFinal = 10.25;
+ // precio final = 11.00 no se puede
+  print (precioFinal);
+
+ // const
+ const pi = 3.1415;
+ // pi = 3.1416 no se puede
+ print (pi);
+
+  
+}
---
+++ clase/03_datos_compuestos.dart
@@ -0,0 +1,12 @@
+void main () {
+
+  // list
+  // list, var, final, const
+  // uns lista es como un cesta, y va a tener cosas internamente
+  // tipo de dato que va  contener la lista
+  
+  List <String> ListaDeCompras = ["Pera","Manzana","Uva"];
+    print (ListaDeCompras);
+    //acceder con la posicion
+    print (ListaDeCompras[1]);
+}
