aedd61f647b991a0b2ad86c3e9f869d7692d3001
Vinizzio <vinizzio@hotmail.com>
2026-03-09 19:05:20 -0500

    clase 7.1

--- clase/07_operadores_logicos.dart
+++ clase/07_operadores_logicos.dart
@@ -1,3 +1,16 @@
 void main() {
-  print("Resultado or ${false || true}");
+  int x = 5;
+  int y = 8;
+  // comparar valores boleanos
+  // AND -> Y -> && -> alt+38
+  // Es verdadero si todo es verdadero
+  // es falso si uno es falso 
+  final comp1 = (5 < 8) && (x != y);
+  print("Resultado comp1: $comp1");
+
+  // OR -> 0 -> || -> alt124
+  // es verdadero si almenos uno es verdadero
+
+  // negacion -> 
+  //print("Resultado or ${false || true}");
 }
