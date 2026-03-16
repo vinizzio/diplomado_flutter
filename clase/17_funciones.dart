11ec5a9119fe905145d13112bb246308ccbf377e
Vinizzio <vinizzio@hotmail.com>
2026-03-15 21:13:28 -0500

    clase 17 y 18

--- clase/17_funciones.dart
+++ clase/17_funciones.dart
@@ -1,18 +1,25 @@
 void main() {
-  sumar();
+  sumar(3, 5);
+  final resultado = sumar2(
+    num2: 3,
+    num1: 2,
+  );
+  print("La suma es: $resultado");
   // 4+4
   // 3+3
   // 6+4
 }
 
-// funcion 
-void sumar(){
-  final suma = 3 + 2;
+// funcion argumentos por posicion
+void sumar(int num1, int num2){
+  final suma = num1 + num2;
   print("La suma es: $suma");
 }
 
 // concepto de retorno
 // creamos la suma pero no la imprimas en la funcion , imprimer en el main
-
-//int sumar2(){
-  
+// argumentos por nombre
+int sumar2({required int num1 , required int num2}) {
+final suma = num1+num2;
+return suma;
+}
---
+++ clase/18_funcion_flecha.dart
@@ -0,0 +1,7 @@
+int sumar (int a , int b) => a + b;
+  void main () {
+    int resultado = sumar(5, 3);
+    print("El resultado es: $resultado");
+  }
+
+// llamado tambien lamdas
